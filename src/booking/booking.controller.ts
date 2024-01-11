d506224f3684a69f51099223a5ea7183f9d97db5
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2024-01-11 14:23:43 +0530

    add skip jwt to public route

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -3,12 +3,14 @@ import { BookingService } from "./booking.service";
 import { BookingDto } from "./dto/booking.dto";
 import { ReturnMessage } from "src/utils/returnType";
 import { FilterDto } from "./dto/filterData.dto";
+import { SkipJwt } from "src/auth/decorators/jwt/skip-jwt.decorator";
 
 
 @Controller("booking")
 export class BookingController {
     constructor(private readonly bookingService: BookingService) { }
-
+    
+    @SkipJwt()
     @Post()
     bookRoom(
         @Body() bookingDetails: BookingDto,
@@ -16,6 +18,7 @@ export class BookingController {
        return this.bookingService.bookRoom(bookingDetails)
     }
 
+    @SkipJwt()
     @Get()
     getAllPropertyDetails(@Query() filterData: FilterDto):Promise<any>{
         return this.bookingService.getAllPropertyDetails(filterData)
