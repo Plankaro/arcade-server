d6009c2aa475086eeb0149f1431cfe811e9b8e01
Rabin karmakar <106877477+rabin97@users.noreply.github.com>
2024-01-11 17:14:37 +0530

    change auth module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { AdminController } from "./admin.controller";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule,   AuthModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
